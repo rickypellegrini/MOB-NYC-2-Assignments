b4b53f898ff1ce46b33dbe73f8f1dbe452929c22
Riccardo Pellegrini <riccardo.g.pellegrini@gmail.com>
2015-02-24 17:43:22 -0500

    Ricky HW for submission

---
+++ week2hw/week2hw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week2hw
+//
+//  Created by Riccardo Pellegrini on 2/24/15.
+//  Copyright (c) 2015 Riccardo Pellegrini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
